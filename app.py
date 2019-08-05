b968bf821e7cfca3c4a84884c1d1d06e0260177f
Daniel Go <godaniel135@gmail.com>
2019-08-05 21:17:40 +0700

    Fix the graphql structure

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_graphql import GraphQLView
-from graphene import ObjectType, Field, Schema, String, List, relay
+from graphene import ObjectType, Field, Schema, String, List, relay, ResolveInfo
 import spacy
 
 app = Flask(__name__)
 CORS(app)
 nlp = spacy.load('id_ud-tag-dep-ner-1.0.0')
+
+
+# JSON Object
+class Prediction(ObjectType):
+    id = String()
+    token = String()
+    label = String()
 
 
 # Model Object --> that represent the structure of the Spacy model and handle the function
@@ -15,37 +22,65 @@ class Model(ObjectType):
         interfaces = (relay.Node, )
 
     text = String()
-    token = List(String)
-    pos = List(String)
-    dep = List(String)
-    ner = List(String)
+    token = List(Prediction)
+    pos = List(Prediction)
+    dep = List(Prediction)
+    ner = List(Prediction)
 
-    def resolve_token(parent, info):
+    def resolve_token(parent, info: ResolveInfo):
         doc = nlp(parent.text)
         result = []
+        predictions = []
+        i=0
         for token in doc:
-            result.append(token.text)
+            predictions.append((i, token.text))
+            i += 1
+        for id, token in predictions:
+            prediction = Prediction(id=id, token=token, label="-")
+            result.append(prediction)
+
         return result
 
-    def resolve_pos(parent, info):
+    def resolve_pos(parent, info: ResolveInfo):
         doc = nlp(parent.text)
         result = []
+        predictions = []
+        i=0
         for token in doc:
-            result.append((token.text,token.pos_))
+            predictions.append((i, token.text, token.pos_))
+            i += 1
+        for id, token, label in predictions:
+            prediction = Prediction(id=id, token=token, label=label)
+            result.append(prediction)
+
         return result
 
-    def resolve_dep(parent, info):
+    def resolve_dep(parent, info: ResolveInfo):
         doc = nlp(parent.text)
         result = []
+        predictions = []
+        i = 0
         for token in doc:
-            result.append((token.text, token.dep_))
+            predictions.append((i, token.text, token.dep_))
+            i += 1
+        for id, token, label in predictions:
+            prediction = Prediction(id=id, token=token, label=label)
+            result.append(prediction)
+
         return result
 
-    def resolve_ner(parent, info):
+    def resolve_ner(parent, info: ResolveInfo):
         doc = nlp(parent.text)
         result = []
+        predictions = []
+        i = 0
         for ent in doc.ents:
-            result.append((ent.text, ent.label_))
+            predictions.append((i, ent.text, ent.label_))
+            i += 1
+        for id, token, label in predictions:
+            prediction = Prediction(id=id, token=token, label=label)
+            result.append(prediction)
+
         return result
 
     @classmethod
